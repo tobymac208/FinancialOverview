a1606a11b2b7c5fa2718cb85c2f5321be052908b
Nik <niklas_fernandez@comcast.net>
2020-09-22 21:25:10 -0500

    moved some stuff around

--- main.py
+++ main.py
@@ -12,16 +12,19 @@ income_item_one = IncomeItem("Alexander Home Services", 530.83)
 income_item_two = IncomeItem("Alexander Home Services", 387.45)
 
 # get some debt items ready
-debt_item_one = DebtItem("Next Level Combat", 130.0)
+debt_item_one = DebtItem("Next Level Combat", 130.00)
 debt_item_two = DebtItem("Next Insurance", 62.30)
 debt_item_three = DebtItem("Hulu", 12.00)
+debt_item_four = DebtItem("Gym", 30.00)
 
 # add the items to the object
 overview.add_income_item(income_item_one)
 overview.add_income_item(income_item_two)
+
 overview.add_debt_item(debt_item_one)
 overview.add_debt_item(debt_item_two)
 overview.add_debt_item(debt_item_three)
+overview.add_debt_item(debt_item_four)
 
 # print out the info
 overview.print_overview()
