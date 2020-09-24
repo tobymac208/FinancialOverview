1602be872fc48915b3630572c899ef9c06dd69f4
Nik <niklas_fernandez@comcast.net>
2020-09-23 20:04:39 -0500

    refactored read functionality

--- main.py
+++ main.py
@@ -4,23 +4,26 @@ from DebtInfo import DebtItem
 from DebtInfo import DebtItemList
 from FinancialOverview import FinancialOverview
 
-# Setup our main object
-overview = FinancialOverview("Nik's finances for the month of September 2020")
+# read in the income and debt data
+def read_data(name_of_overview):
+    object = FinancialOverview(name_of_overview)
+    with open("income.txt", 'r', encoding = 'utf-8') as file:
+        # load in income data
+        for line in file:
+            split_string = line.split(',')
+            # use the data to create a new IncomeItem
+            new_income_item = IncomeItem(split_string[0], float(split_string[1]))
+            # add the new item
+            object.add_income_item(new_income_item)
+    with open('debt.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            split_string = line.split(',')
+            new_debt_item = DebtItem(split_string[0], float(split_string[1]))
+            object.add_debt_item(new_debt_item)
+    return object
 
-# load in data
-with open("income.txt", 'r', encoding = 'utf-8') as file:
-    # load in income data
-    for line in file:
-        split_string = line.split(',')
-        # use the data to create a new IncomeItem
-        new_income_item = IncomeItem(split_string[0], float(split_string[1]))
-        # add the new item
-        overview.add_income_item(new_income_item)
-with open('debt.txt', 'r', encoding='utf-8') as file:
-    for line in file:
-        split_string = line.split(',')
-        new_debt_item = DebtItem(split_string[0], float(split_string[1]))
-        overview.add_debt_item(new_debt_item)
+# Setup our main object
+overview = read_data("Nik's finances for the month of September 2020")
 
 # print out the info
 overview.print_overview()
