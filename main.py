6043f53275f492f852049ce5c47872b6c1b4a2b3
Nik <niklas_fernandez@comcast.net>
2020-09-23 19:59:36 -0500

    moved data over to files

--- main.py
+++ main.py
@@ -22,25 +22,6 @@ with open('debt.txt', 'r', encoding='utf-8') as file:
         new_debt_item = DebtItem(split_string[0], float(split_string[1]))
         overview.add_debt_item(new_debt_item)
 
-# get some income items ready
-# income_item_one = IncomeItem("Alexander Home Services", 530.83)
-# income_item_two = IncomeItem("Alexander Home Services", 387.45)
-
-# get some debt items ready
-# debt_item_one = DebtItem("Next Level Combat", 130.00)
-# debt_item_two = DebtItem("Next Insurance", 62.30)
-# debt_item_three = DebtItem("Hulu", 12.00)
-# debt_item_four = DebtItem("Gym", 30.00)
-
-# add the items to the object
-# overview.add_income_item(income_item_one)
-# overview.add_income_item(income_item_two)
-
-# overview.add_debt_item(debt_item_one)
-# overview.add_debt_item(debt_item_two)
-# overview.add_debt_item(debt_item_three)
-# overview.add_debt_item(debt_item_four)
-
 # print out the info
 overview.print_overview()
 
